8ab7093d73ca174b48946967a62138de3aec2c52
wormchenli <wormchenli@gmail.com>
2026-02-02 13:01:11 -0700

    added global filter to dbcontext

--- IdentityService.Infrastructure/IdentityDbContext.cs
+++ IdentityService.Infrastructure/IdentityDbContext.cs
@@ -1,4 +1,6 @@
-﻿using IdentityService.Domain.Entities;
+﻿using CommonUtils;
+using DomainCommons.Models.Interfaces;
+using IdentityService.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,5 +16,8 @@ public class IdentityDbContext:IdentityDbContext<User, Role, string>
     {
         base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+        
+        // apply global query filter for soft delete
+        builder.EnableSoftDeleteFilter<ISoftDelete>(propertyName:"IsDeleted");
     }
 }
