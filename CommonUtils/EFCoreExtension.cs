bfed5ea0b09a0ec798a11d6a19894ea33d7e7a5e
wormchenli <wormchenli@gmail.com>
2026-02-02 10:58:20 -0700

    finished global soft-delete filter

--- CommonUtils/EFCoreExtension.cs
+++ CommonUtils/EFCoreExtension.cs
@@ -16,13 +16,27 @@ public static class EFCoreExtension
     /// <param name="propertyName">Soft-delete flag property name. Defaults to <c>IsDeleted</c>.</param>
     public static void EnableSoftDeleteFilter<TInterface>(this ModelBuilder builder, string propertyName = "IsDeleted")
     {
+        // 1. find entities that implement TInterface
         var entityTypesWithSoftDeletion = builder.Model.GetEntityTypes()
             .Where( e=> e.ClrType.IsAssignableTo(typeof(TInterface)));
-
+        // 2. iterate, construct filter expression, and apply
         foreach (var entityType in entityTypesWithSoftDeletion)
         {
             var property = entityType.FindProperty(propertyName);
+            // Expression start: p =>
+            
             var parameter = Expression.Parameter(entityType.ClrType, "p");
+            // p.IsDeleted
+            
+            var memberExpression = Expression.Property(parameter, property!.PropertyInfo!);
+            // !p.IsDeleted
+            
+            var notExpression = Expression.Not(memberExpression);
+            // Final lambda: p => !p.IsDeleted  (equivalent to p.IsDeleted == false)
+            
+            var lambda = Expression.Lambda(notExpression, parameter);
+            builder.Entity(entityType.ClrType).HasQueryFilter(lambda);
+            // or entityType.SetQueryFilter(lambda);
         }
     }
 }
